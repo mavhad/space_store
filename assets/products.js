8581d35ce41fe54285bf8721a2a544a042935b9c
Mrunal <mrunalavhad272000@gmail.com>
2021-10-31 23:10:24 +0530

    cart and animation

---
+++ assets/products.js
@@ -0,0 +1,88 @@
+const products = [
+    {
+      id: 0,
+      name: "Book",
+      price: 00,
+      instock: 100,
+      description:
+        "Learn about Satellite.",
+      imgSrc: "/assets/book1.jpg",
+    },
+    {
+      id: 1,
+      name: "T-shirt 2",
+      price: 00,
+      instock: 43,
+      description:
+        "Comfortable T-shirts.",
+      imgSrc: "/assets/shirt1.jpg",
+    },
+    {
+      id: 2,
+      name: "Part1",
+      price: 00,
+      instock: 10,
+      description:
+        "Parts For Satellite",
+      imgSrc: "/assets/part1.jpg",
+    },
+    {
+      id: 3,
+      name: "T-shirt 4",
+      price: 00,
+      instock: 5,
+      description:
+        "Comfortable T-shirts.",
+      imgSrc: "/assets/shirt3.jpg",
+    },
+    {
+      id: 4,
+      name: "Rocket_Model",
+      price: 00,
+      instock: 4,
+      description:
+        "Parts",
+      imgSrc: "/assets/rocket_model.jpg",
+    },
+    {
+      id: 5,
+      name: "Part",
+      price: 00,
+      instock: 40,
+      description:
+        "Part To build a satellite.",
+      imgSrc: "/assets/part2.jpg",
+    },
+
+    {
+        id: 6,
+        name: "Book",
+        price: 00,
+        instock: 40,
+        description:
+          "Amazing Books.",
+        imgSrc: "/assets/Book2.jpg",
+      },
+
+      {
+        id: 7,
+        name: "Astronaut Kit",
+        price: 00,
+        instock: 40,
+        description:
+          "Amazing Kit.",
+        imgSrc: "/assets/kit.jpg",
+      },
+
+      {
+        id: 8,
+        name: "Part",
+        price: 00,
+        instock: 40,
+        description:
+          "Amazing Books.",
+        imgSrc: "/assets/part3.jpg",
+      },
+    
+  ];
+  
